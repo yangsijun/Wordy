1dca6f8d5d7ba20aa457265167b18b8b71e302e2
Yang Sijun <yang@sijun.dev>
2025-03-13 17:22:42 +0900

    Initial Commit

---
+++ WordyTests/WordyTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordyTests.swift
+//  WordyTests
+//
+//  Created by 양시준 on 3/13/25.
+//
+
+import Testing
+@testable import Wordy
+
+struct WordyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
