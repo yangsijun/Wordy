9a2f37fca96c29b62295d3fe25a47dbf042689c7
Yang Sijun <yang@sijun.dev>
2025-03-19 23:54:29 +0900

    Implement QuizView and QuizButtonGroupView

---
+++ Wordy/View/QuizButtonGroupView.swift
@@ -0,0 +1,111 @@
+//
+//  QuizButtonGroupView.swift
+//  Wordy
+//
+//  Created by 양시준 on 3/19/25.
+//
+
+import SwiftUI
+import UIKit
+
+struct QuizButtonGroupView: View {
+    var quizzes: [Quiz]
+    @Binding var quiz: Quiz
+    @Binding var quizIndex: Int
+    
+    @Binding var selectedOption: String?
+    @Binding var isCorrect: Bool?
+    @Binding var showNextQuestion: Bool
+    
+    var body: some View {
+        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 2), spacing: 8) {
+            ForEach(quiz.options, id: \.self) { option in
+                if isCorrect == nil {
+                    Button(action: {
+                        handleAnswer(option)
+                    }) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color(UIColor.secondarySystemBackground))
+                                .stroke(Color(UIColor.systemFill), lineWidth: 2)
+                                .frame(minHeight: 80)
+                            Text(option)
+                                .foregroundColor(Color(UIColor.secondaryLabel))
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .padding()
+                        }
+                    }
+                } else {
+                    Button(action: {
+                        handleAnswer(option)
+                    }) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(
+                                    option == quiz.answer
+                                        ? Color(UIColor.systemTeal).opacity(0.3)
+                                        : Color(UIColor.secondarySystemBackground)
+                                )
+                                .stroke(
+                                    option == quiz.answer
+                                        ? Color(UIColor.systemTeal)
+                                        : option == selectedOption
+                                            ? Color(UIColor.systemOrange)
+                                            : Color(UIColor.systemFill),
+                                    lineWidth: 3
+                                )
+                                .frame(minHeight: 80)
+                            Text(option)
+                                .foregroundColor(Color(UIColor.secondaryLabel))
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .padding()
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func handleAnswer(_ option: String) {
+        selectedOption = option
+        isCorrect = option == quiz.answer
+        
+        showNextQuestion = true
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            nextQuiz()
+        }
+    }
+    
+    func nextQuiz() {
+        selectedOption = nil
+        isCorrect = nil
+        showNextQuestion = false
+        
+        quizIndex += 1
+        if quizIndex >= quizzes.count {
+            quizIndex = 0
+        }
+        quiz = quizzes[quizIndex]
+    }
+}
+
+
+#Preview {
+    QuizButtonGroupView(
+        quizzes: [
+            Quiz(question: "this is a test1", options: ["a", "b", "c", "d"], answer: "a"),
+            Quiz(question: "this is a test2", options: ["a", "b", "c", "d"], answer: "a"),
+            Quiz(question: "this is a test3", options: ["a", "b", "c", "d"], answer: "a"),
+            Quiz(question: "this is a test4", options: ["a", "b", "c", "d"], answer: "a"),
+            Quiz(question: "this is a test5", options: ["a", "b", "c", "d"], answer: "a")
+        ],
+        quiz: .constant(Quiz(question: "this is a test", options: ["a", "b", "c", "d"], answer: "a")),
+        quizIndex: .constant(0),
+        selectedOption: .constant(nil),
+        isCorrect: .constant(nil),
+        showNextQuestion: .constant(false)
+    )
+}
---
+++ Wordy/View/QuizView.swift
@@ -0,0 +1,90 @@
+//
+//  QuizView.swift
+//  Wordy
+//
+//  Created by 양시준 on 3/19/25.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var quizzes: [Quiz]
+    @State var quiz: Quiz
+    @State var quizIndex: Int = 0
+    
+    @State private var selectedOption: String? = nil
+    @State private var isCorrect: Bool? = nil
+    @State private var showNextQuestion = false
+    
+    
+    let correctMessages = [
+        "Spot on! 🎯 Well done, let’s crack on with the next one.",
+        "Brilliant! 👏 You’re on a roll.",
+        "Top marks! ✅ Keep up the great work.",
+        "Smashing effort! 🎉 Let’s see what’s next.",
+        "Nicely done! 🌟 Fancy another challenge?"
+    ]
+    
+    let incorrectMessages = [
+        "Unlucky! ❌ Give it another go next time.",
+        "Not quite! 🤔 Don’t worry, you’re learning!",
+        "A valiant effort! 💪 Let’s have another try.",
+        "Oh dear! 😅 It happens to the best of us.",
+        "Close, but no cigar! 🎭 On to the next one."
+    ]
+    
+    init (quizzes: [Quiz]) {
+        self.quizzes = quizzes
+        self.quiz = quizzes[0]
+        self.quizIndex = 0
+    }
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .top) {
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color(UIColor.systemFill), lineWidth: 2)
+                    .background(.thinMaterial)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .overlay(
+                        Text(quiz.question)
+                            .font(.largeTitle)
+                    )
+                if (showNextQuestion) {
+                    VStack {
+                        Spacer()
+                            .frame(height: 50)
+                        Text(isCorrect == true ? correctMessages.randomElement()! : incorrectMessages.randomElement()!)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 40)
+                            .frame(width: .infinity, alignment: .center)
+                    }
+                }
+            }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+            QuizButtonGroupView(
+                quizzes: quizzes,
+                quiz: $quiz,
+                quizIndex: $quizIndex,
+                selectedOption: $selectedOption,
+                isCorrect: $isCorrect,
+                showNextQuestion: $showNextQuestion
+            )
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+        }
+    }
+}
+
+#Preview {
+    QuizView(
+        quizzes: [
+            Quiz(question: "this is a test1", options: ["a", "b", "c", "d"], answer: "a"),
+            Quiz(question: "this is a test2", options: ["a", "b", "c", "d"], answer: "a"),
+            Quiz(question: "this is a test3", options: ["a", "b", "c", "d"], answer: "a"),
+            Quiz(question: "this is a test4", options: ["a", "b", "c", "d"], answer: "a"),
+            Quiz(question: "this is a test5", options: ["a", "b", "c", "d"], answer: "a")
+        ]
+    )
+}
