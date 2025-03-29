dc4ab53a9d2469832660fa6eb26cc547ec531814
Yang Sijun <yang@sijun.dev>
2025-03-29 15:50:35 +0900

    Implement Search feature in WordGroupView

--- Wordy/View/WordGroupView.swift
+++ Wordy/View/WordGroupView.swift
@@ -10,10 +10,16 @@ import SwiftData
 
 struct WordGroupView: View {
     @State var wordGroup: WordGroup!
+    @State var searchQueryString: String = ""
     
     var body: some View {
         NavigationStack {
-            List(wordGroup.words.sorted(by: { $0.id < $1.id }), id: \.id) { word in
+            List(
+                searchQueryString.isEmpty
+                  ? wordGroup.words.sorted(by: { $0.id < $1.id })
+                  : wordGroup.words.filter { $0.word.localizedStandardContains(searchQueryString) }.sorted(by: { $0.id < $1.id }),
+                id: \.id
+            ) { word in
                 NavigationLink(destination: WordView(word: word)) {
                     HStack {
                         Text(word.word)
@@ -26,6 +32,11 @@ struct WordGroupView: View {
             .navigationTitle(wordGroup.title)
             .navigationBarTitleDisplayMode(.large)
         }
+        .searchable(
+            text: $searchQueryString,
+            placement: .navigationBarDrawer(displayMode: .always),
+            prompt: Text("Search")
+        )
     }
 }
 
@@ -54,24 +65,27 @@ struct WordGroupView: View {
                 ]
             ),
             WordItem(
-                id: "abandon_1",
-                word: "abandon",
-                pos: "verb",
-                phonetics: ["/əˈbændən/"],
+                id: "connected",
+                word: "connected",
+                pos: "adjective",
+                phonetics: ["/kəˈnektɪd/"],
                 senses: [
                     WordSense(
-                        id: "abandon_sng_1",
+                        id: "connected_sng_1",
                         group: ["ox3000"],
-                        cefr: "b2",
-                        meaning: "to leave somebody, especially somebody you are responsible for, with no intention of returning",
+                        cefr: "A2",
+                        meaning: "(of two or more things or people) having a link between them",
                         examples: [
-                            "The baby had been abandoned by its mother.",
-                            "People often simply abandon their pets when they go abroad.",
-                            "‘We have been abandoned to our fate,’ said one resident.",
-                            "The study showed a deep fear among the elderly of being abandoned to the care of strangers."
-                        ]
-                    )
-                ]
+                           "We do not believe that the incidents are connected.",
+                           "The two issues are closely connected.",
+                           "market prices and other connected matters",
+                           "The fall in retail sales is directly connected with the decline in employment.",
+                           "jobs connected with the environment",
+                           "Many computer files connected to the case have been erased.",
+                           "They are connected by marriage."
+                       ]
+                   )
+               ]
             )
         ]
     ))
